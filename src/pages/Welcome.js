46341021efff5b402c264803f1ca762fef2ff963
Shironene <yanason.ys2016@gmail.com>
2024-04-03 01:24:10 +0700

    Make it better

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -11,9 +11,24 @@ function Welcome() {
         <div>      
         <header className="App-header">
             <HomeNavbar/>
-            <div className="main-container">
-                <h1>Welcome to JDGCSE Pet Shop!</h1>
-                <Button variant="primary" className='mx-2' onClick={() => {navigate("/products")}}>Buy now</Button>
+            <div style={{
+                 display: 'flex', 
+                 alignItems: 'center', 
+                 justifyContent: 'space-around', 
+                 padding: '50px', 
+                 marginTop: '50px',
+                 backgroundColor: '#FFEBCD' }
+            }>
+                <div>
+                    <h1 style={{ color: '#FF8C00', fontWeight: 'bold', fontSize: '48px', marginBottom: '20px' }}>Preparing Your Home For Your Buddy</h1>
+                    <p style={{ color: '#555', marginBottom: '30px' }}>Welcome to our pet-friendly community! 
+                        Whether you're welcoming a new furry friend into your home or simply looking 
+                        to enhance your pet's living environment.</p>
+                    <Link to="/products"><Button variant="success" style={{width:"400px", height: "100px", fontSize: "50px"}}>Buy Now</Button></Link>
+                </div>
+                <div>
+                    <img src="https://i.ytimg.com/vi/rdN4i1xZPw0/maxresdefault.jpg"></img>
+                </div>
             </div>
         </header>
         </div>
